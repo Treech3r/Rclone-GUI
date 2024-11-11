a487c6e1eeadd0b2d948e88bc6c7cc4d3ced0a40
Treecher <187679618+Treech3r@users.noreply.github.com>
2024-11-11 18:01:53 -0300

    fix: get available drive letters on Windows

--- lib/utils/windows.dart
+++ lib/utils/windows.dart
@@ -1,11 +1,13 @@
 import 'shell.dart';
 
 Future<List<String>> getAvailableDriveLetters() async {
-  const powershellCommand =
-      'wmic logicaldisk get caption | findstr /r "^[A-Z]"';
+  const powershellCommand = 'wmic logicaldisk get caption';
 
   var lettersCurrentlyInUse =
       (await runShellCommand(powershellCommand)).stdout as String;
+
+  lettersCurrentlyInUse = lettersCurrentlyInUse.replaceAll(':', '');
+  lettersCurrentlyInUse.replaceFirst('Caption', '');
 
   return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
       .split('')
