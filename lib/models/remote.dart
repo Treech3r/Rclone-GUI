75b2d0c885531e0dcb79226d3ca8e3f37095ebc3
Treech3r <187679618+Treech3r@users.noreply.github.com>
2025-01-09 19:52:00 -0300

    refactor: add commercial name of most remotes

--- lib/models/remote.dart
+++ lib/models/remote.dart
@@ -13,12 +13,60 @@ class Remote {
   }
 
   String get getCommercialName {
-    switch (type) {
-      case 'drive':
-        return 'Google Drive';
-      default:
-        return 'Unknown';
-    }
+    Map<String, String> commercialNames = {
+      'drive': 'Google Drive',
+      'dropbox': 'Dropbox',
+      'mega': 'Mega',
+      'fichier': '1Fichier',
+      'netstorage': 'NetStorage',
+      's3': 'Amazon S3',
+      'b2': 'Backblaze B2',
+      'box': 'Box',
+      'sharefile': 'Citrix Sharefile',
+      'koofr': 'Koofr',
+      'filefabric': 'Enterprise File Fabric',
+      'filescom': 'Files.com',
+      'ftp': 'ftp',
+      'gofile': 'Gofile',
+      'google cloud storage': 'Google Cloud Storage',
+      'google photos': 'Google Photos',
+      'hdfs': 'Hadoop distributed file system',
+      'hidrive': 'HiDrive',
+      'http': 'HTTP',
+      'internetarchive': 'Internet Archive',
+      'jottacloud': 'Jottacloud',
+      'linkbox': 'Linkbox',
+      'mailru': 'Mail.ru Cloud',
+      'azureblob': 'Microsoft Azure Blob Storage',
+      'azurefiles': 'Microsoft Azure Files Storage',
+      'onedrive': 'Microsoft OneDrive',
+      'swift': 'OpenStack Swift',
+      'opendrive': 'OpenDrive',
+      'oracleobjectstorage': 'Oracle Cloud Infrastructure Object Storage',
+      'pcloud': 'Pcloud',
+      'pikpak': 'PikPak',
+      'pixeldrain': 'Pixeldrain Filesystem',
+      'premiumizeme': 'premiumize.me',
+      'putio': 'Put.io',
+      'protondrive': 'Proton Drive',
+      'qingstor': 'QingStor Object Storage',
+      'quatrix': 'Quatrix by Maytech',
+      'seafile': 'Seafile',
+      'sftp': 'SSH/SFTP',
+      'sia': 'Sia Decentralized Cloud',
+      'smb': 'SMB',
+      'storj': 'Storj Decentralized Cloud Storage',
+      'sugarsync': 'Sugarsync',
+      'ulozto': 'Uloz.to',
+      'uptobox': 'Uptobox',
+      'webdav': 'WebDAV',
+      'yandex': 'Yandex Disk',
+      'zoho': 'Zoho',
+      'local': 'Arquivos locais',
+      'memory': 'Arquivos na memória RAM'
+    };
+
+    return commercialNames[type] ?? 'Desconhecido';
   }
 
   String get getCommercialLogo {
