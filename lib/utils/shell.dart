3f7dc0d03c2facfdfb8706337c84d73b3ae11226
Treech3r <187679618+Treech3r@users.noreply.github.com>
2025-06-18 15:43:39 -0300

    refactor: wrap shell util function with SystemShell class

--- lib/utils/shell.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:process_run/shell.dart';
-
-Future<ProcessResult> runShellCommand(String command) async {
-  var shell = Shell();
-  return (await shell.run(command)).first;
-}
